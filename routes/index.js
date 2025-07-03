3a7f283e6216ce33cf5c0638606395cf1412d321
Jerome <nietsemorej@gmail.com>
2025-07-03 13:35:45 -0700

    Implement form handling with validation in Express app. Added Pug templates for rendering registration form and layout. Updated dependencies in package.json and package-lock.json to include body-parser, express-validator, and pug.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
 
 router.get('/', function(req, res) {
-  res.send('It works!');
+    res.render('form', {title: 'Registration Form'});
+});
+
+router.post('/', 
+    [
+        check('name').isLength({ min: 1 }).withMessage('Please enter a name'),
+        check('email').isEmail().withMessage('Please enter a valid email address'),
+    ]
+    ,
+    function(req, res) {
+        console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render('form', {title: 'Registration Form', name: req.body.name, email: req.body.email, errors: errors.array(), data: req.body});
+        } else {
+            res.send('Thank you for your registration!');
+        }
 });
 
 module.exports = router; 
