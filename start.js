f5ac1fa4546288211bf65b2ab656949f17cd5386
Jerome <nietsemorej@gmail.com>
2025-07-10 15:07:28 -0700

    Set up MongoDB connection using dotenv for environment variables. Added DATABASE configuration in .env file and updated package.json and package-lock.json to include mongoose and dotenv dependencies.

--- start.js
+++ start.js
@@ -1,5 +1,23 @@
-const app = require('./app');
+// start.js
+require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.
+on('open', () => {
+  console.log('Mogoose connection open');
+});
+
+on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+const app = require('./app');
 const server = app.listen(3000, function() {
   console.log(`Express is running on port ${server.address().port}`);
 }); 
